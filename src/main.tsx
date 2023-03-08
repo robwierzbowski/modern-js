bbf0486d4bdd6400a23d9a0aeff398925bc9db44
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-03-08 11:02:42 -0600

    Try switching to pure nodenext/ ES modules!

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { App } from './App.js';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
