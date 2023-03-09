3467adf247ba85a1c35844ede6fd7186cd13af61
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-03-09 12:00:36 -0600

    Add some next step TODOs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// TODO:
+// - Create unified target for ES2020 or modern browsers (using browserslist?)
+//   for JS and CSS
+// - Find out what level Vite compiles to by default
+// - Find out how minimal we can make module imports in prod Vite
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
