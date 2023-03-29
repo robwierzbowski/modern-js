61b034208d519c5e2c9a3cdb2ba195c17c94cd40
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-03-29 10:29:48 -0500

    Remove unnecessary React import
    EsBuild/Vite are automatically inserting the JSX transform.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { App } from './App/index.js';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
