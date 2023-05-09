2a026342c46f1304c379cdca1c0b391acf7f63da
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-05-09 12:15:59 -0500

    Update robBnB

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,62 @@
-import { config } from './eslint-config/index.js';
+// To work with the new flat config, the TypeScript parser module must be
+// imported as a single object
+// eslint-disable-next-line import/no-namespace
+import * as typescriptParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+import { robBnBConfig } from 'eslint-robbnb';
+import globals from 'globals';
+
+const languageOptions = {
+  globals: {
+    // Adding browser and node globals to all files for convenience
+    ...globals.browser,
+    ...globals.node,
+    ...globals.es2021,
+  },
+  // For reduced complexity we can ignore Babel and use
+  // @typescript-eslint/parser to parse both TS and JS files
+  parser: typescriptParser,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    // Assume that apps are using the new JSX runtime auto insertion
+    // https://typescript-eslint.io/architecture/parser/#jsxpragma
+    jsxPragma: null,
+    project: './tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: '.',
+  },
+};
+
+const config = [
+  ...robBnBConfig,
+
+  {
+    ignores: ['dist/*'],
+  },
+
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx', '**/*.d.ts'],
+    languageOptions,
+    linterOptions: {
+      // Log a warning when eslint-disable comments exist for inactive rules
+      reportUnusedDisableDirectives: true,
+    },
+  },
+
+  {
+    files: ['**/*.test.js', '**/*.test.ts', '**/*.test.tsx', '**/test/**'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+
+  prettierConfig,
+];
 
 // eslint-disable-next-line import/no-default-export
 export default config;
