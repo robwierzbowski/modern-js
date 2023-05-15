4851eabebe4d8a2a1fda7567842438f6e87423a0
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-05-15 12:52:48 -0500

    Require assertions for a test to pass

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,14 @@
-import { expect, afterEach } from 'vitest';
+import { expect, afterEach, beforeEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
 
 // Extend Vitest with matchers from react-testing-library
 expect.extend(matchers);
+
+// Require at least one expect per it block
+beforeEach(() => {
+  expect.hasAssertions();
+});
 
 // Cleanup after each test case (e.g., resetting jsdom)
 afterEach(() => {
