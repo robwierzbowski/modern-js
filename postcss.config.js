8b2b0795f1889bc01bf6a28943bb659ac488a120
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-08-15 12:34:33 -0500

    Add postcss-mixins, test css module variables (yes I know this should be two commits)

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,10 @@
 import autoprefixer from 'autoprefixer';
+import postcssMixins from 'postcss-mixins';
 import postcssNested from 'postcss-nested';
 
+// Setup minimal SCSS-like additions to CSS. Prefer native custom properties
+// over preprocessed variables.
 // eslint-disable-next-line import/no-default-export
 export default {
-  plugins: [autoprefixer, postcssNested],
+  plugins: [autoprefixer, postcssMixins, postcssNested],
 };
