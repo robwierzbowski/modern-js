1b0f90fdc814fb4f3836a30d1cf93b8a7ee5f334
Rob Wierzbowski <rob.wierzbowski@gmail.com>
2023-11-17 11:58:31 -0600

    Upgrade to Vite 5

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,15 @@ const plugins: PluginOption[] = [react()];
 
 if (process.env.ANALYZE === 'true') {
   plugins.push(
+    // Rollup plugin visualizer suggests type coercion in their docs. Not
+    // pretty, but for now it's ok.
+    // https://github.com/btd/rollup-plugin-visualizer
     visualizer({
       brotliSize: true,
       filename: './dist/bundle-analysis.html',
       gzipSize: true,
       open: true,
-    }),
+    }) as unknown as PluginOption,
   );
 }
 
